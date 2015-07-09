460ed80105942c1d73d2675818eb7890886bfd84
mattythomas <mattythomas2000@gmail.com>
2015-07-09 15:26:16 +0100

    Revert "Parent and sub projects on project domain entity"
    This reverts commit 47f51be7de9bb755bb9d5b64efaab4ba480ab3bc.

--- src/TeamCitySharp/DomainEntities/Project.cs
+++ src/TeamCitySharp/DomainEntities/Project.cs
@@ -13,7 +13,6 @@
         public string Id { get; set; }
         public string Name { get; set; }
         public string WebUrl { get; set; }
-        public string ParentProjectId { get; set; }
 
         public BuildTypeWrapper BuildTypes { get; set; }
         public Parameters Parameters { get; set; }
